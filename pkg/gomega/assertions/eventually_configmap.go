a372a59357455fc69c1f1ebfa80db469eb399b30
Ryan Williams <3375653+ryankwilliams@users.noreply.github.com>
2023-06-14 06:46:19 -0400

    Modify gomega configmap assertion docstring (#10)
    * Modify gomega configmap assertion docstring
    
    Removes the statement mentioning polling until success as its not
    implemented by the assertion.
    
    * Update pkg/gomega/assertions/eventually_configmap.go
    
    Co-authored-by: jbpratt <jbpratt78@gmail.com>
    
    ---------
    
    Co-authored-by: jbpratt <jbpratt78@gmail.com>

--- pkg/gomega/assertions/eventually_configmap.go
+++ pkg/gomega/assertions/eventually_configmap.go
@@ -10,9 +10,8 @@ import (
 
 // EventuallyConfigMap is a gomega async assertion that can be used with the
 // standard or custom gomega matchers
-// Polls the resource every 30 seconds until success or timeout defined by POLLING_TIMEOUT
 //
-//	EventuallyConfigMap(ctx, client, configMapName, namespace).Should(Not(BeNil()), "config map %s should exist", configMapName)
+//	EventuallyConfigMap(ctx, client, configMapName, namespace).ShouldNot(BeNil()), "config map %s should exist", configMapName)
 func EventuallyConfigMap(ctx context.Context, client *openshift.Client, name, namespace string) gomega.AsyncAssertion {
 	return gomega.Eventually(ctx, func(ctx context.Context) (*corev1.ConfigMap, error) {
 		var configMap corev1.ConfigMap
