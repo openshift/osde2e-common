94ba2f02af34c669e4d66294cf5ed1f72f92f736
Brady Pratt <bpratt@redhat.com>
2023-06-14 05:51:30 -0500

    pkg/clients/openshift: implement impersonation
    allows a client to impersonate another user/serviceaccount to test
    permissions
    
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- pkg/clients/openshift/client.go
+++ pkg/clients/openshift/client.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/openshift/api"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/e2e-framework/klient/conf"
 	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 )
@@ -29,4 +30,30 @@ func NewFromKubeconfig(filename string) (*Client, error) {
 		return nil, fmt.Errorf("unable to register openshift api schemes: %w", err)
 	}
 	return &Client{client}, nil
+}
+
+// Impersonate returns a copy of the client with a new ImpersonationConfig
+// established on the underlying client, acting as the provided user
+//
+//	backplaneUser, _ := oc.Impersonate("test-user@redhat.com", "dedicated-admins")
+func (c *Client) Impersonate(user string, groups ...string) (*Client, error) {
+	if user != "" {
+		// these groups are required for impersonating a user
+		groups = append(groups, "system:authenticated", "system:authenticated:oauth")
+	}
+
+	client := *c
+	newRestConfig := rest.CopyConfig(c.Resources.GetConfig())
+	newRestConfig.Impersonate = rest.ImpersonationConfig{UserName: user, Groups: groups}
+	newResources, err := resources.New(newRestConfig)
+	if err != nil {
+		return nil, err
+	}
+	client.Resources = newResources
+
+	if err = api.Install(client.GetScheme()); err != nil {
+		return nil, fmt.Errorf("unable to register openshift api schemes: %w", err)
+	}
+
+	return &client, nil
 }
