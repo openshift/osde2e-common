7324b03fd8e5f795bc11898d33ab8e3a87b4b3e5
Ritu Mundhe <rmundhe@redhat.com>
2024-01-11 12:58:41 -0600

    matcher for olm.owner label

--- pkg/gomega/matchers/contain_item_with_prefix_test.go
+++ pkg/gomega/matchers/contain_item_with_prefix_test.go
@@ -12,18 +12,20 @@ var _ = Describe("list", func() {
 	It("should contain the item", func() {
 		list := &rbacv1.RoleList{
 			Items: []rbacv1.Role{
-				{ObjectMeta: metav1.ObjectMeta{Name: "test123"}},
+				{ObjectMeta: metav1.ObjectMeta{Name: "test123", Labels: map[string]string{"olm.owner": "abc123"}}},
 			},
 		}
 		Expect(list).Should(ContainItemWithPrefix("test"))
+		Expect(list).Should(ContainItemWithOLMOwnerWithPrefix("abc"))
 	})
 
 	It("should not contain the item", func() {
 		list := &appsv1.DeploymentList{
 			Items: []appsv1.Deployment{
-				{ObjectMeta: metav1.ObjectMeta{Name: "brady"}},
+				{ObjectMeta: metav1.ObjectMeta{Name: "brady", Labels: map[string]string{"olm.owner": "ritu"}}},
 			},
 		}
 		Expect(list).ShouldNot(ContainItemWithPrefix("test"))
+		Expect(list).ShouldNot(ContainItemWithOLMOwnerWithPrefix("test"))
 	})
 })
