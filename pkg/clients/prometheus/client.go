8595e9caed195ed90beceb440692aa488d19f07d
Brady Pratt <bpratt@redhat.com>
2024-05-30 06:03:32 -0500

    clients/prometheus: use library-go
    a better implementation is provided in library-go which correctly
    creates the prometheus client via a service account token with proper
    ssl configuration.
    
    the deps are pinned at release-4.16:
    
    ```
    go get github.com/openshift/api@release-4.16
    ```
    
    https://github.com/openshift/library-go/blob/354b673304be747827a7873426952da68f96b5c5/test/library/metrics/query.go#L27
    
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- pkg/clients/prometheus/client.go
+++ pkg/clients/prometheus/client.go
@@ -2,21 +2,16 @@ package prometheus
 
 import (
 	"context"
-	"crypto/tls"
 	"errors"
 	"fmt"
-	"net"
-	"net/http"
-	"net/url"
-	"strings"
 	"time"
 
-	routev1 "github.com/openshift/api/route/v1"
+	routev1client "github.com/openshift/client-go/route/clientset/versioned"
+	"github.com/openshift/library-go/test/library/metrics"
 	"github.com/openshift/osde2e-common/pkg/clients/openshift"
-	"github.com/prometheus/client_golang/api"
 	prometheusv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
-	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 type Client struct {
@@ -25,53 +20,23 @@ type Client struct {
 
 // TODO: should we use thanos querier instead?
 func New(ctx context.Context, client *openshift.Client) (*Client, error) {
-	var route routev1.Route
-	if err := client.Get(ctx, "prometheus-k8s", "openshift-monitoring", &route); err != nil {
-		return nil, fmt.Errorf("unable to find prometheus route: %w", err)
-	}
-	address := "https://" + route.Spec.Host
-
-	var secretList corev1.SecretList
-	if err := client.WithNamespace("openshift-monitoring").List(ctx, &secretList); err != nil {
-		return nil, fmt.Errorf("failed to list secrets: %w", err)
+	cfg := client.GetConfig()
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, err
 	}
 
-	var bearerToken string
-	for _, secret := range secretList.Items {
-		if strings.HasPrefix(secret.Name, "prometheus-k8s-token") {
-			bearerToken = string(secret.Data[corev1.ServiceAccountTokenKey])
-			break
-		}
-	}
-	if len(bearerToken) == 0 {
-		return nil, fmt.Errorf("failed to find token secret for prometheus-k8s serviceaccount")
+	routeClient, err := routev1client.NewForConfig(cfg)
+	if err != nil {
+		return nil, err
 	}
 
-	// TODO: can this be done differently?
-	cfg := api.Config{
-		Address: address,
-		RoundTripper: &http.Transport{
-			Proxy: func(request *http.Request) (*url.URL, error) {
-				request.Header.Add("Authorization", "Bearer "+bearerToken)
-				return http.ProxyFromEnvironment(request)
-			},
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-			}).DialContext,
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-			TLSHandshakeTimeout: 10 * time.Second,
-		},
-	}
-
-	promClient, err := api.NewClient(cfg)
+	prometheus, err := metrics.NewPrometheusClient(ctx, kubeClient, routeClient)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create prometheus client: %w", err)
 	}
 
-	return &Client{prometheusv1.NewAPI(promClient)}, nil
+	return &Client{prometheus: prometheus}, nil
 }
 
 func (c *Client) InstantQuery(ctx context.Context, query string) (model.Vector, error) {
