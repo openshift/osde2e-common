eab7edfa5d69af2de6e169dc79508f065b75c088
Diego Santamaria <dsantama@redhat.com>
2024-06-18 10:14:05 -0600

    Fixed conditional for both token and Client secrets, added rosa whoami to verifylogin to validate client works behind proxy and vpn corp.

--- pkg/openshift/rosa/rosa.go
+++ pkg/openshift/rosa/rosa.go
@@ -202,6 +202,11 @@ func verifyLogin(ctx context.Context, rosaBinary string, token string, clientID 
 		return fmt.Errorf("login failed with %q: %w", stderr, err)
 	}
 
+	_, stderr, err = cmd.Run(exec.CommandContext(ctx, rosaBinary, "whoami"))
+	if err != nil {
+		return fmt.Errorf("whoami failed with %q: %w", stderr, err)
+	}
+
 	return nil
 }
 
@@ -209,7 +214,7 @@ func verifyLogin(ctx context.Context, rosaBinary string, token string, clientID 
 // to openshift cluster manager "ocm". It is the callers responsibility
 // to close the ocm connection when they are finished (defer provider.Connection.Close())
 func New(ctx context.Context, token string, clientID string, clientSecret string, ocmEnvironment ocmclient.Environment, logger logr.Logger, args ...*awscloud.AWSCredentials) (*Provider, error) {
-	if ocmEnvironment == "" || (token == "" || (clientID == "" && clientSecret == "")) {
+	if ocmEnvironment == "" || (token == "" && (clientID == "" || clientSecret == "")) {
 		return nil, &providerError{err: errors.New("some parameters are undefined, unable to construct osd provider")}
 	}
 
